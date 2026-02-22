6799cd87f290fe35f724f98f36c92802b9ba73d0
Leroy Ramaphoko <adventapp777@gmail.com>
2026-02-22 16:59:52 +0200

    daily-verse.proto version.proto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,58 @@
+import com.google.protobuf.gradle.*
+
 plugins {
-    kotlin("jvm") version "2.3.0"
+    kotlin("jvm") version "2.0.21"
+    `maven-publish`
+    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "com.biblecontentmedia.church.protos"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    api("com.google.protobuf:protobuf-kotlin:3.25.5")
+    api("io.grpc:grpc-kotlin-stub:1.4.1")
+    api("io.grpc:grpc-protobuf:1.69.0")
+    api("javax.annotation:javax.annotation-api:1.3.2")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 }
 
 kotlin {
     jvmToolchain(21)
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    withSourcesJar()
+}
+
+protobuf {
+    protoc { artifact = "com.google.protobuf:protoc:3.25.5" }
+    plugins {
+        id("grpc") { artifact = "io.grpc:protoc-gen-grpc-java:1.69.0" }
+        id("grpckt") { artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.1:jdk8@jar" }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            task.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            artifactId = "church-protos"
+        }
+    }
 }
